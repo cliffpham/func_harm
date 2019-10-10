cd33666ea67c97f5f69581bd3b83b0f5c9e7c183
cliffpham <cliffpham@gmail.com>
2019-10-10 01:18:08 -0700

    reworked skeleton for styling, tweaked functionality of chord selection, updated TODO

--- client/src/components/Central.vue
+++ client/src/components/Central.vue
@@ -1,23 +1,34 @@
 <template>
   <b-container>
-    <b-row class="text-center" style="height: 200px;">
-      <b-col offset-md="1">
-        <div id="keySelector">
-          {{key}}
+
+    <b-row class="text-center" style="margin-bottom: 2%; border: dotted 1px #000;">
+      <b-col offset-md="1" align-self="center">
+        <div style="display: inline-block;">
+          <div style="display: flex; flex-direction: row;">
+            <div class="selectors">*Tempo</div>
+            <div class="selectors">*Mixtures</div>
+            <div class="selectors" v-on:click="getTones">Major or Minor</div>
+            <div class="selectors">
+              <b-form-select v-model="key" size="lg">
+                <option v-for="key in signatures" :value="key">{{key}}</option>
+              </b-form-select>
+            </div>
+          </div>
         </div>
-        <div>
-          <b-form-select v-model="key">
-            <option v-for="key in signatures" :value="key">{{key}}</option>
-          </b-form-select>
-        </div>
-        <b-button :pressed.sync="majorOrMinor" v-on:click="getTones()" variant="primary">Toggle Me</b-button>
-        <p>Major: <strong>{{ majorOrMinor }}</strong></p>
       </b-col>
-      <b-col cols="8">
+    </b-row>
+
+    <b-row class="text-center" style="margin-bottom: 2%;">
+      <b-col align-self="center">
+        <div id="keySelector">
+          <div id="keyS"><h1 style="font-size: 5em;">{{key}}</h1></div>
+        </div>
+      </b-col>
+      <b-col align-self="center">
         <b-row>
-          <div class="chord" v-on:click="playOrAdd(0)">{{tones[0]}}</div>
-          <div class="chord" v-on:click="playOrAdd(2)">{{tones[2]}}</div>
-          <div class="chord" v-on:click="playOrAdd(5)">{{tones[5]}}</div>
+          <div class="chord tonic" v-on:click="playOrAdd(0)">{{tones[0]}}</div>
+          <div class="chord tonic" v-on:click="playOrAdd(2)">{{tones[2]}}</div>
+          <div class="chord tonic" v-on:click="playOrAdd(5)">{{tones[5]}}</div>
         </b-row>
         <b-row>
           <div class="chord" v-on:click="playOrAdd(3)">{{tones[3]}}</div>
@@ -29,91 +40,106 @@
         </b-row>
       </b-col>
     </b-row>
-    <b-row class="text-center">
-      <b-col>
-        <h1> Selected Chords </h1>
-        <div id="selectedChords">
-          <div v-for="chord in selectedChords" class="chord">{{chord}}
-        </div>
-        <b-button v-on:click="removeChord"> Remove </b-button>
-        <b-button v-on:click="playChords" variant="primary"> Play </b-button>
-        </div>
+
+    <b-row class="text-center" style="border: dotted 1px #000;">
+      <b-col align-self="start">
+          <div style="display: flex; flex-direction: row;">
+            <div id="selectedChords">
+              <div class="buttons" v-on:click="playChords"><font-awesome-icon icon="play"/></div>
+            <div class="buttons" v-on:click="removeChord"><font-awesome-icon icon="trash"/></div>
+            </div>
+            <div id="selectedChords">
+              <div v-for="chord in selectedChords"
+                   class="selectedChord"
+                   v-on:click="play(chord[0])">
+                  <h6>{{chord[1]}}</h6>
+                  <p>{{chord[0]}}</p>
+              </div>
+          </div>
+          </div>
       </b-col>
     </b-row>
+
   </b-container>
 </template>
 
 <script>
-import * as IS from "isnext";
+import * as IS from 'isnext';
 import * as player from '../player';
 
 export default {
   name: 'Central',
   data() {
     return {
-      key: 'E',
+      key: 'C',
       majorOrMinor: true,
       i: 0,
       tones: IS.progression.major,
       signatures: ['C', 'F', 'Bb', 'Eb', 'A', 'D', 'Gb', 'Cb', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#'],
       selectedChords: [],
       numClicks: 0,
-    }
+    };
   },
   methods: {
-    getTones(){
-      if (this.majorOrMinor){
-        this.tones = IS.progression.major;
-      } else {
+    getTones() {
+      if (this.majorOrMinor) {
         this.tones = IS.progression.minor;
+        this.majorOrMinor = false;
+      } else {
+        this.tones = IS.progression.major;
+        this.majorOrMinor = true;
       }
     },
-    findChord(i){
-      let tones;
-      let result;
-      result = IS.progression.get_chord_for_function(this.tones[i], this.key); 
-      return result
+    findChord(i) {
+      const result = IS.progression.get_chord_for_function(this.tones[i], this.key);
+      return result;
     },
-    playChords(){
-      player.play_progression(this.selectedChords);
+    play(chord) {
+      player.play_chord(chord);
     },
-    removeChord(){
+    playChords() {
+      const temp = [];
+      for (let i = 0; i < this.selectedChords.length; i += 1) {
+        temp.push(this.selectedChords[i][0]);
+      }
+      player.play_progression(temp);
+    },
+    removeChord() {
       this.selectedChords.pop();
     },
-    playOrAdd(i){
+    playOrAdd(i) {
       const result = this.findChord(i);
-      this.numClicks += 1
-      if (this.numClicks === 1){
+      const funcHarm = this.tones[i];
+      this.numClicks += 1;
+      if (this.numClicks === 1) {
         const self = this;
         setTimeout(() => {
           switch (self.numClicks) {
-              case 1:
-                player.play_chord(result);
-                break;
-              default:
-                this.selectedChords.push(result);
+            case 1:
+              player.play_chord(result);
+              break;
+            default:
+              self.selectedChords.push([result, funcHarm]);
           }
           self.numClicks = 0;
         }, 200);
       }
-    }
+    },
   },
 };
 </script>
 
 <style>
 #keySelector {
-  width: 76px;
-  height: 75px;
+  width: 150px;
+  height: 150px;
+  display: inline-block;
+  border: dotted 3px #000;
 }
 
 #selectorChords {
   display: flex;
   flex-direction: row;
-}
-
-#keySelector {
-  border: dotted 3px #000;
 }
 
 #chords {
@@ -122,7 +148,47 @@ export default {
   justify-content: center;
 }
 
+#keyS {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+}
+
+.selectors {
+  width: 100px;
+  height: 50px;
+  border: dotted 1px #100;
+  margin: 2px;
+  text-align: center;
+}
+
+.buttons {
+  width: 50px;
+  height: 50px;
+  border: dotted 1px #000;
+  margin-top: 2px;
+  margin-bottom: 2px;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 50px;
+}
+
 .chord {
+  width: 150px;
+  height: 50px;
+  border: dotted 1px #000;
+  margin: 2px;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 50px;
+}
+
+.tonic {
+}
+
+.selectedChord {
   width: 50px;
   height: 50px;
   border: dotted 1px #000;
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash, faPlay } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +11,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
+library.add(faTrash);
+library.add(faPlay);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
